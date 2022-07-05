5474e45fd2f2a84621359429af35290c36d01752
Andres De la Hoz <andelahozg@gmail.com>
2022-07-04 19:02:55 -0500

    Implementing API connected with robot framework

--- app.py
+++ app.py
@@ -1,15 +1,62 @@
-from flask import Flask, jsonify, request
+from tkinter.tix import Tree
+from attr import field
+from flask import Flask, jsonify, request, abort
 from flask_restful import Resource, Api
+from marshmallow import Schema, fields
+from tasks import open_the_website, search_for, look_best_fit_company, look_for_companies
+
+
+class CompanyQuerySchema(Schema):
+    company = fields.String(required=True)
+    location = fields.String(required=True)
+    top_searchs = fields.Integer(required=True)
+
 
 app = Flask(__name__)
-api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
 
-api.add_resource(HelloWorld, '/')
+schema = CompanyQuerySchema()
+
+# spec = APISpec(
+#     title= 'flask-api-swagger-doc',
+#     version= '1.0.0',
+#     openapi_version = '3.0.2'
+#     plugins = [FlaskPuging(), MarshallowPlugin()]
+# )
+
+
+
+@app.route('/search_all_companies', methods=['GET'])
+def get():
+    errors = schema.validate(request.args)
+    if not errors:
+         abort(errors, 422)
+    company = request.args.get('company')
+    department = request.args.get('department')
+    top_searchs = int(request.args.get('top_searchs'))
+    open_the_website("https://www.einforma.co/buscador-empresas-empresarios")
+    search_for(company, department)
+    look_for_companies(top_searchs, company)
+    results = look_for_companies(top_searchs, company)
+    return jsonify(results)
+
+
+@app.route('/search_best_fit_company', methods=['GET'])
+def get():
+    errors = schema.validate(request.args)
+    if not errors:
+         abort(errors, 422)
+    company = request.args.get('company')
+    department = request.args.get('department')
+    top_searchs = int(request.args.get('top_searchs'))
+    open_the_website("https://www.einforma.co/buscador-empresas-empresarios")
+    search_for(company, department)
+    look_for_companies(top_searchs, company)
+    results = look_best_fit_company(top_searchs, company)
+    return jsonify(results)
+
+
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, use_debugger=False, use_reloader=True)
--- tasks.py
+++ tasks.py
@@ -3,12 +3,13 @@ from tkinter.messagebox import NO
 from turtle import end_fill
 from RPA.Browser.Selenium import Selenium
 import business_table
+import json
 
 browser_lib = Selenium()
 
 
 def open_the_website(url):
-    browser_lib.open_available_browser(url)
+    browser_lib.open_available_browser(url, headless=True)
 
 
 def search_for(company, location):
@@ -17,40 +18,59 @@ def search_for(company, location):
     browser_lib.press_keys(input_field, "ENTER")
     browser_lib.select_from_list_by_label("id:PROVINCIA", location)
 
-def look_for_companies(top_results):
+def look_for_companies(top_results, company_search):
     companies_founded = browser_lib.get_text('xpath://*[@id="a_nacional"]')
-    
+    companies_list = []
+    data = {"companies": companies_list}
     if companies_founded == "Empresas y Empresarios (0)":
-        print("Resultados no encontrados" )
-        return "Resultados no encontrados" 
-
+        companies_list.append("Resultados no encontrados")
+        return json.dumps(data)
     best_score = 100
     actual_row = 0
     html_table = browser_lib.get_element_attribute("css:table#nacional", "outerHTML")
     table = business_table.read_table_from_html(html_table)
-
     while actual_row < top_results:
         for row in table:
             if actual_row >= top_results:
                 break
             actual_company = row[0]
             actual_score = best_score
+
+
             for char in actual_company:
-                if char not in actual_company:
+                if char not in company_search:
                     actual_score -= 1
             browser_lib.click_element(f'xpath://h3[contains(text(), "{row[0]}")]')
             store_screenshot(f"output/{row[0]}.png")
-            print(store_result())
+            companies_list.append(store_result(actual_score))
             browser_lib.go_back()
             actual_row += 1
+    data_json = data
+  
+    return data_json
 
-            
-        
+
+def look_best_fit_company(top_searchs, company_name):
+    data = look_for_companies(top_searchs, company_name)
+    best_score_fit = 0
+    best_dict_fit = {}
+    for dicti in data['companies']:
+        for key, value in dicti.items():
+            if key == 'score_similitud':
+                if value > best_score_fit:
+                    best_score_fit = value
+                    best_dict_fit = dicti
+
+    best_fit = best_dict_fit
+    return best_fit
+
+
 
 def store_screenshot(filename):
     browser_lib.screenshot(filename=filename)
 
-def store_result():
+
+def store_result(score_similitud):
     ICI = browser_lib.get_text('xpath://*[@id="imprimir"]/table/tbody/tr[2]/td[2]/a') if browser_lib.is_element_visible('xpath://*[@id="imprimir"]/table/tbody/tr[2]/td[2]/a') else None
     NIT = browser_lib.get_text('xpath://*[@id="imprimir"]/table/tbody/tr[3]/td[2]/a') if browser_lib.is_element_visible('xpath://*[@id="imprimir"]/table/tbody/tr[3]/td[2]/a') else None
     razon_social = browser_lib.get_text('xpath://*[@id="imprimir"]/table/tbody/tr[4]/td[2]') if browser_lib.is_element_visible('xpath://*[@id="imprimir"]/table/tbody/tr[4]/td[2]') else None
@@ -80,16 +100,22 @@ def store_result():
         "matricula_mercantil": matricula_mercantil,
         "ultimo_balance_einforma": ultimo_balance_einforma,
         "fecha_ultimo_dato": fecha_ultimo_dato,
-        "ultimo_balance_einforma": fecha_actualizacion_camara_comercio
+        "ultimo_balance_einforma": fecha_actualizacion_camara_comercio,
+        "score_similitud": score_similitud
     }
     return result_dict
 
 # Define a main() function that calls the other functions in order:
 def main():
     try:
+        company_name = "baires".capitalize()
+        location = "BOGOTÁ"
+        top_searchs = 3
         open_the_website("https://www.einforma.co/buscador-empresas-empresarios")
-        search_for("baires", "BOGOTÁ")
-        look_for_companies(9)
+        search_for(company_name, location)
+        #look_for_companies(top_searchs, company_name)
+        look_best_fit_company(top_searchs, company_name)
+        
     finally:
         browser_lib.close_all_browsers()
 
